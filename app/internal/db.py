d522ae0c7e9aed05622c4d7362ed49f6f5569a23
marcelaksouza <ma.karoline@hotmail.com>
2023-03-26 19:29:26 +0100

    DB connection + seeds.py

--- app/internal/db.py
+++ app/internal/db.py
@@ -9,14 +9,17 @@ base_dir = pathlib.Path(__file__).parent.parent.parent
 load_dotenv(base_dir.joinpath('.env'))
 
 class Config:
+    DB_ENDPOINT = os.getenv('DB_ENDPOINT')
     DB_REGION_NAME = os.getenv('DB_REGION_NAME')
     DB_ACCESS_KEY_ID = os.getenv('DB_ACCESS_KEY_ID')
     DB_SECRET_ACCESS_KEY = os.getenv('DB_SECRET_ACCESS_KEY')
 
-def initialize_db() -> ServiceResource:
+def Initialize_db() -> ServiceResource:
     ddb = boto3.resource('dynamodb',
-                         region_name=Config.DB_REGION_NAME,
-                         aws_access_key_id=Config.DB_ACCESS_KEY_ID,
-                         aws_secret_access_key=Config.DB_SECRET_ACCESS_KEY)
-
+                        endpoint_url=Config.DB_ENDPOINT,
+                        region_name=Config.DB_REGION_NAME,
+                        aws_access_key_id=Config.DB_ACCESS_KEY_ID,
+                        aws_secret_access_key=Config.DB_SECRET_ACCESS_KEY
+                        )
+    print("DB connected")
     return ddb
--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 # import asyncio
+# import uvicorn
 import graphene
-import uvicorn
 from fastapi import FastAPI
 from starlette_graphene3 import GraphQLApp, make_playground_handler #,make_graphiql_handler
 
+from app.internal.db import Initialize_db
+from app.internal.seeds import add_seeds
+
 app = FastAPI(title='ServerlessAP', description='GraphQL APIs', version='0.1')
+
+ddb = Initialize_db()
+
+#To populate DB with seeds
+add_seeds(ddb)
 
 @app.get("/")
 async def root():
@@ -16,12 +24,11 @@ class Query(graphene.ObjectType):
     def resolve_hello(self, info, name):
         return "Hello " + name
 
-
 schema = graphene.Schema(query=Query)
 
 # app.mount("/", GraphQLApp(schema, on_get=make_graphiql_handler()))  # Graphiql IDE
 app.mount("/graphql", GraphQLApp(schema, on_get=make_playground_handler()))  # Playground IDE
 # app.mount("/", GraphQLApp(schema)) # no IDE
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info", reload=True)
+# if __name__ == '__main__':
+#     uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info", reload=True)
